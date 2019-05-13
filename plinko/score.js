ef459ccc06258004cdf9515d1b5eda2ea9ddb516
Suranart Niamcome <suranartnc@gmail.com>
2019-05-13 18:11:03 +0700

    Find the accuracy

--- plinko/score.js
+++ plinko/score.js
@@ -7,8 +7,34 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
+  const testSetSize = 10
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize)
+
+  // For way
+  // let numberCorrect = 0
+
+  // for (let i =0; i < testSet.length; i++) {
+  //   const bucket = knn(trainingSet, testSet[i][0])
+  //   if (bucket === testSet[i][3]) {
+  //     numberCorrect++
+  //   }
+  // }
+
+  // console.log(`Accuracy: ${numberCorrect / testSetSize}`)
+  
+  // Lodash way
+  const accuracy = _.chain(testSet)
+    .filter(testPoint => knn(trainingSet, testPoint[0]) === testPoint[3])
+    .size()
+    .divide(testSetSize)
+    .value()
+
+  console.log('Accuracy:', accuracy)
+}
+
+function knn(data, point) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -18,10 +44,17 @@ function runAnalysis() {
     .first()
     .parseInt()
     .value()
-
-  console.log(`Point ${predictionPoint} will probably fall into bucket ${bucket}`)
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint)
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB)
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data)
+
+  const testSet = _.slice(shuffled, 0, testCount)
+  const trainingSet = _.slice(shuffled, testCount)
+
+  return [testSet, trainingSet]
 }
