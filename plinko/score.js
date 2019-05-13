7728b51cce13f7e41ca4d34438c252271ff8b0a6
Suranart Niamcome <suranartnc@gmail.com>
2019-05-13 18:50:40 +0700

    Loop through dynamic k

--- plinko/score.js
+++ plinko/score.js
@@ -1,6 +1,5 @@
 const outputs = []
 const predictionPoint = 300
-const k = 3
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel])
@@ -9,30 +8,19 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 function runAnalysis() {
   const testSetSize = 10
   const [testSet, trainingSet] = splitDataset(outputs, testSetSize)
-
-  // For way
-  // let numberCorrect = 0
-
-  // for (let i =0; i < testSet.length; i++) {
-  //   const bucket = knn(trainingSet, testSet[i][0])
-  //   if (bucket === testSet[i][3]) {
-  //     numberCorrect++
-  //   }
-  // }
-
-  // console.log(`Accuracy: ${numberCorrect / testSetSize}`)
   
-  // Lodash way
-  const accuracy = _.chain(testSet)
-    .filter(testPoint => knn(trainingSet, testPoint[0]) === testPoint[3])
-    .size()
-    .divide(testSetSize)
-    .value()
-
-  console.log('Accuracy:', accuracy)
+  _.range(1, 15).forEach(k => {
+    const accuracy = _.chain(testSet)
+      .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+      .size()
+      .divide(testSetSize)
+      .value()
+  
+    console.log(`Accuracy(k = ${k}):, ${accuracy}`)
+  })
 }
 
-function knn(data, point) {
+function knn(data, point, k) {
   return _.chain(data)
     .map(row => [distance(row[0], point), row[3]])
     .sortBy(row => row[0])
