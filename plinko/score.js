00fb2eee81d91c5838006ca305a36659427d37b7
Suranart Niamcome <suranartnc@gmail.com>
2019-05-14 15:43:45 +0700

    Implement KNN for multiple features

--- plinko/score.js
+++ plinko/score.js
@@ -6,12 +6,12 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const testSetSize = 10
+  const testSetSize = 100
   const [testSet, trainingSet] = splitDataset(outputs, testSetSize)
   
-  _.range(1, 15).forEach(k => {
+  _.range(1, 20).forEach(k => {
     const accuracy = _.chain(testSet)
-      .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+      .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3])
       .size()
       .divide(testSetSize)
       .value()
@@ -22,7 +22,12 @@ function runAnalysis() {
 
 function knn(data, point, k) {
   return _.chain(data)
-    .map(row => [distance(row[0], point), row[3]])
+    .map(row => {
+      return [
+        distance(_.initial(row), point), 
+        _.last(row)
+      ]
+    })
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -35,7 +40,11 @@ function knn(data, point, k) {
 }
 
 function distance(pointA, pointB) {
-  return Math.abs(pointA - pointB)
+  return _.chain(pointA)
+    .zip(pointB)
+    .map(([a, b]) => (a - b) ** 2)
+    .sum()
+    .value() ** 0.5
 }
 
 function splitDataset(data, testCount) {
