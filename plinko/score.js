365d9466d90c335e9ee15205e3f8c370f8808bc5
Suranart Niamcome <suranartnc@gmail.com>
2019-05-14 18:39:16 +0700

    Evaluate different feature values

--- plinko/score.js
+++ plinko/score.js
@@ -7,16 +7,20 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
 function runAnalysis() {
   const testSetSize = 100
-  const [testSet, trainingSet] = splitDataset(minMax(outputs, 3), testSetSize)
-  
-  _.range(1, 20).forEach(k => {
+  const k = 10
+
+  _.range(0, 3).forEach(feature => {
+    const data = _.map(outputs, row => [row[feature], _.last(row)])
+    const [testSet, trainingSet] = splitDataset(minMax(data, 1), testSetSize)
     const accuracy = _.chain(testSet)
-      .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3])
+      .filter(
+        testPoint => knn(trainingSet, _.initial(testPoint), k) === _.last(testPoint)
+      )
       .size()
       .divide(testSetSize)
       .value()
   
-    console.log(`Accuracy(k = ${k}):, ${accuracy}`)
+    console.log(`Accuracy(feature = ${feature}):, ${accuracy}`)
   })
 }
 
