8ba936657f78c5dd2923889e7f6aa4e85ca45a81
Suranart Niamcome <suranartnc@gmail.com>
2019-05-17 14:25:20 +0700

    Running a KNN

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -2,6 +2,22 @@ require('@tensorflow/tfjs-node')
 const tf = require('@tensorflow/tfjs')
 const loadCSV = require('./load-csv')
 
+const k = 10
+
+function knn(features, labels, predictionPoint, k) {
+  return features
+    .sub(predictionPoint)
+    .pow(2)
+    .sum(1)
+    .pow(0.5)
+    .expandDims(1)
+    .concat(labels, 1)
+    .unstack()
+    .sort((a, b) => a.get(0) > b.get(0) ? 1 : -1)
+    .slice(0, k)
+    .reduce((acc, pair) => acc + pair.get(1), 0) / k
+}
+
 let { features, labels, testFeatures, testLabels } = loadCSV('./kc_house_data.csv', {
   shuffle: true,
   splitTest: 10,
@@ -9,5 +25,8 @@ let { features, labels, testFeatures, testLabels } = loadCSV('./kc_house_data.cs
   labelColumns: ['price']
 })
 
-console.log(testFeatures)
-console.log(testLabels)
+features = tf.tensor(features)
+labels = tf.tensor(labels)
+
+const result = knn(features, labels, tf.tensor(testFeatures[0]), k)
+console.log('Guess', result, testLabels[0][0])
